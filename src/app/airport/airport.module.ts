cc5f0a8f5908017a79da88abf38e05380459756f
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-27 00:49:22 +0300

    Added airport routing module

---
+++ src/app/airport/airport.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AirportRoutingModule } from './airport-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AirportRoutingModule
+  ],
+  declarations: []
+})
+export class AirportModule { }
