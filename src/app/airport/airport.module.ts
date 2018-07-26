ecd567ff76085bf3312ad63048c18dcf15ed364e
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-27 00:56:53 +0300

    Added material navbar 'ng generate @angular/material:material-nav --name navbar'

--- src/app/airport/airport.module.ts
+++ src/app/airport/airport.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AirportRoutingModule } from './airport-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    AirportRoutingModule
+    AirportRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
-  declarations: []
+  declarations: [NavbarComponent]
 })
 export class AirportModule { }
