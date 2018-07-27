56015c3efbbfb557fcfd2e23e44a5072f7c68982
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-27 19:26:25 +0300

    Added AirplaneTypeService

--- src/app/shared/services/airplanetype.service.ts
+++ src/app/shared/services/airplanetype.service.ts
@@ -1,7 +1,40 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AirplaneType} from '../models/airplanetype';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class AirplanetypeService {
 
-  constructor() { }
+  private url='http://pizzaService';
+  private endpoint = 'pizzas';
+
+  constructor(protected httpClient: HttpClient) {
+
+  }
+
+  public create(airplaneType: AirplaneType): Observable<AirplaneType> {
+    return this.httpClient
+      .post<AirplaneType>(`${this.url}/${this.endpoint}`, airplaneType);
+  }
+
+  public update(airplaneType: AirplaneType): Observable<AirplaneType> {
+    return this.httpClient
+      .put<AirplaneType>(`${this.url}/${this.endpoint}/${airplaneType.id}`, airplaneType);
+  }
+
+  read(id: number): Observable<AirplaneType> {
+    return this.httpClient
+      .get<AirplaneType>(`${this.url}/${this.endpoint}/${id}`);
+  }
+
+  list(): Observable<AirplaneType[]> {
+    return this.httpClient
+      .get<AirplaneType[]>(`${this.url}/${this.endpoint}`);
+  }
+
+  delete(id: number) {
+    return this.httpClient
+      .delete(`${this.url}/${this.endpoint}/${id}`);
+  }
 }
