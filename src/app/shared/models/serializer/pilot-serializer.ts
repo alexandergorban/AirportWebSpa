f0e5f590b0b0c4efed21f67678a11035427432a1
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-28 11:45:18 +0300

    Updated pilot service

---
+++ src/app/shared/models/serializer/pilot-serializer.ts
@@ -0,0 +1,24 @@
+import {Pilot} from '../pilot';
+
+export class PilotSerializer {
+  fromJson(json: any): Pilot {
+    const pilot = new Pilot();
+    pilot.id = json.id;
+    pilot.name = json.name;
+    pilot.surname = json.surname;
+    pilot.dateOfBirth = json.dateOfBirth;
+    pilot.experience = json.experience;
+
+    return pilot;
+  }
+
+  toJson(pilot: Pilot): any {
+    return {
+      id: pilot.id,
+      name: pilot.name,
+      surname: pilot.surname,
+      dateOfBirth: pilot.dateOfBirth,
+      experience: pilot.experience
+    };
+  }
+}
--- src/app/shared/services/pilot.service.ts
+++ src/app/shared/services/pilot.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ResourceService} from './base/resource.service';
+import {Pilot} from '../models/pilot';
+import {PilotSerializer} from '../models/serializer/pilot-serializer';
 
 @Injectable()
-export class PilotService {
-
-  constructor() { }
+export class PilotService extends ResourceService<Pilot>{
+  constructor(httpClient: HttpClient) {
+    super(httpClient,
+      'http://localhost:32157/api/v1',
+      'pilots',
+      new PilotSerializer())
+  }
 }
