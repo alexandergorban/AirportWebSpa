4ef315fed94e87837050734400d5bb263ae29831
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-28 11:46:09 +0300

    Updated stewardess service

---
+++ src/app/shared/models/serializer/stewardess-serializer.ts
@@ -0,0 +1,23 @@
+import {Stewardess} from '../stewardess';
+
+export class StewardessSerializer {
+  fromJson(json: any): Stewardess {
+    const stewardess = new Stewardess();
+    stewardess.id = json.id;
+    stewardess.name = json.name;
+    stewardess.surname = json.surname;
+    stewardess.dateOfBirth = json.dateOfBirth;
+
+    return stewardess;
+  }
+
+  toJson(stewardess: Stewardess): any {
+    return {
+      id: stewardess.id,
+      name: stewardess.name,
+      surname: stewardess.surname,
+      dateOfBirth: stewardess.dateOfBirth,
+    };
+  }
+}
+
--- src/app/shared/services/stewardess.service.ts
+++ src/app/shared/services/stewardess.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ResourceService} from './base/resource.service';
+import {Stewardess} from '../models/stewardess';
+import {StewardessSerializer} from '../models/serializer/stewardess-serializer';
 
 @Injectable()
-export class StewardessService {
-
-  constructor() { }
+export class StewardessService extends ResourceService<Stewardess>{
+  constructor(httpClient: HttpClient) {
+    super(httpClient,
+      'http://localhost:32157/api/v1',
+      'stewardesses',
+      new StewardessSerializer())
+  }
 }
