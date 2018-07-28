246aee38a88f9798493ae99bbc9945d39b9a7014
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-28 11:01:40 +0300

    Added airplanetype service

--- src/app/shared/services/airplanetype.service.ts
+++ src/app/shared/services/airplanetype.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AirplaneType} from '../models/airplanetype';
 import {Observable} from 'rxjs';
