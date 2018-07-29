b58491712ccc4658a10ac70fd4dff7d45e171bd1
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-29 05:38:08 +0300

    Updated departure component

--- src/app/airport/departure/departure.component.ts
+++ src/app/airport/departure/departure.component.ts
@@ -1,34 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import {DepartureService} from '../../shared/services/departure.service';
+import {Departure} from '../../shared/models/departure';
 
 @Component({
   selector: 'departure',
   templateUrl: './departure.component.html',
-  styleUrls: ['./departure.component.css'],
+  styleUrls: ['./departure.component.less'],
 })
 export class DepartureComponent implements OnInit {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  departures: Departure[];
 
   constructor(private breakpointObserver: BreakpointObserver, private departureService: DepartureService) {
 
@@ -36,7 +17,7 @@ export class DepartureComponent implements OnInit {
 
   ngOnInit(): void {
     this.departureService.list().subscribe(data => {
-      debugger;
+      this.departures = data;
     });
   }
 }
