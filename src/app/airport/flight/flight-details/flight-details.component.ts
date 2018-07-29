0d404c871b1bef45468d44c65162b5be0fceef9e
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-29 12:56:07 +0300

    Updated flight details component

--- src/app/airport/flight/flight-details/flight-details.component.ts
+++ src/app/airport/flight/flight-details/flight-details.component.ts
@@ -1,33 +1,23 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {Component, OnInit} from '@angular/core';
+import {FlightService} from '../../../shared/services/flight.service';
+import {Flight} from '../../../shared/models/flight';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'flight-details',
   templateUrl: './flight-details.component.html',
   styleUrls: ['./flight-details.component.css'],
 })
-export class FlightDetailsComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+export class FlightDetailsComponent implements OnInit {
+  flight: Flight;
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  constructor(private route:ActivatedRoute, private flightService: FlightService) {
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  }
+
+  ngOnInit() {
+    this.flightService.read(this.route.snapshot.params['id']).subscribe(data => {
+      this.flight = data;
+    });
+  }
 }
